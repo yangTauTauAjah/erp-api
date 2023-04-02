4a1e33a73bfa9c06d0e0c64a7683400f53c701fd
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-02 10:16:35 +0700

    create controller for "items" route

---
+++ src/modules/items/router.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as controller from "./controllers/index.js";
+
+const router = Router();
+
+router.get("/", controller.read);
+router.post("/", controller.create);
+
+export default router;
